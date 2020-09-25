e0165f53fac6bb5ab2136aecb8a68ec6ad098a1c
Ritwik-Saxena <ritwiksaxena19@gmail.com>
2020-09-25 17:28:52 +0530

    Update Aboutus

--- frontend/src/components/screens/Aboutus.js
+++ frontend/src/components/screens/Aboutus.js
@@ -10,43 +10,46 @@ const Aboutus = () => {
       <div className="row">
         <div className="col s4 m4 l4 offset-l4"></div>
       </div>
-
-      <div className="row">
-        <div className="col m6 s12 ">
-          <img
-            src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80"
-            className="responsive-img"
-            style={{ width: 500 }}
-          />
-        </div>
-        <div className="col m6 s12 ">
-          <h4>It all started with the idea of Code Fellas! </h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quas
-            fugit excepturi unde soluta. Saepe, adipisci fugit deserunt earum
-            voluptates tempora maxime. Eum placeat nemo autem ullam praesentium
-            eius non neque. Corrupti consectetur iste quo accusantium, animi
-            mollitia vero! Nihil
+      <div className="container" style={{ display: "flex", justifyContent: "space-between", marginTop: "9%", marginBottom: "9%" }}>
+        <div className="row">
+          <div className="col m6 s12 ">
+            <img
+              src="https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80"
+              className="responsive-img"
+              style={{ width: 500, height: 300 }}
+            />
+          </div>
+          <div className="col m6 s12 ">
+            <h4>It all started with the idea of Code Fellas! </h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quas
+              fugit excepturi unde soluta. Saepe, adipisci fugit deserunt earum
+              voluptates tempora maxime. Eum placeat nemo autem ullam praesentium
+              eius non neque. Corrupti consectetur iste quo accusantium, animi
+              mollitia vero! Nihil
           </p>
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col m6 s12 ">
-          <h4>What we do?</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quas
-            fugit excepturi unde soluta. Saepe, adipisci fugit deserunt earum
-            voluptates tempora maxime. Eum placeat nemo autem ullam praesentium
-            eius non neque. Corrupti consectetur iste quo accusantium, animi
-            mollitia vero! Nihil
+      <div className="container" style={{ display: "flex", justifyContent: "space-between" }}>
+        <div className="row">
+          <div className="col m6 s12 ">
+            <h4>What we do?</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum quas
+              fugit excepturi unde soluta. Saepe, adipisci fugit deserunt earum
+              voluptates tempora maxime. Eum placeat nemo autem ullam praesentium
+              eius non neque. Corrupti consectetur iste quo accusantium, animi
+              mollitia vero! Nihil
           </p>
-        </div>
-        <div className="col m6 s12 ">
-          <img
-            src="https://images.unsplash.com/photo-1521185496955-15097b20c5fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=647&q=80"
-            className="responsive-img"
-            style={{ width: 500 }}
-          />
+          </div>
+          <div className="col m6 s12 ">
+            <img
+              src="https://images.unsplash.com/photo-1521185496955-15097b20c5fe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=647&q=80"
+              className="responsive-img"
+              style={{ width: 500, height: 300 }}
+            />
+          </div>
         </div>
       </div>
     </section>
