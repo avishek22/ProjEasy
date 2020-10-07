f250c2cfb4dcd7cc2415b723c0d8c9e28f9b3318
Avishek <45457938+avishek22@users.noreply.github.com>
2020-10-07 16:15:36 +0530

    Delete keys.js

--- Backend/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  MONGOURI:
-    "mongodb+srv://avishek:avishek22@cluster0.l1uhs.mongodb.net/CodeFellas?retryWrites=true&w=majority",
-
-  JWT_SECRET: "uiwahldhwahlaospfclm",
-};
